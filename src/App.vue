91d718b07fdb9cb57b86269479385b37f5a8afbe
jackqj888 <11325176@qq.com>
2021-03-22 23:12:11 +0800

    导航组件

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,40 @@
 <template>
   <div id="app">
-      <router-view></router-view>
+     <header>
+       <Navbar />
+     </header>
       </div>
 </template>
 
 <script>
+// import Login from './components/Login'
+import Navbar from './components/Navbar'
 
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    // Login
+    Navbar
+  }
 
 }
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'montserrat', sans-serif;
+}
+
+header {
+  width: 100%;
+  background-color: #222;
+  padding: 15px;
+}
 
 </style>
